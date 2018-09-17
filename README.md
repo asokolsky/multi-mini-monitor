82858f334cc3ea2818053be8fd263a9c528b0f72
Alex Sokolsky <alexander_sokolsky@amat.com>
2018-09-17 16:27:40 -0700

    Menu added.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # multi-mini-monitor
 
 This is an [electron](https://electronjs.org) application presenting the realtime status of the multiple remote hosts exposed by multiple remote [systeminformation-api](https://github.com/asokolsky/systeminformation-api) REST services.
+
+# Installing
+
+```bash
+# Clone this repository
+git clone https://github.com/asokolsky/multi-mini-monitor.git
+# Go into the repository
+cd multi-mini-monitor
+# Install dependencies
+npm install
+# Run the app
+npm start
+```
--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu} = require('electron');
 const minimist = require('minimist');
+
+const menuTemplate = [
+  {
+  label: 'File',
+  submenu: [
+    {
+      label: 'Exit',
+      accelerator: 'CmdOrCtrl+Q',
+      click () { 
+        app.quit() 
+      }
+    }
+  ]
+  }, 
+  {
+  label: 'Help',
+  submenu: [{
+    label: 'About',
+    accelerator: 'F1',
+    click () { 
+      app.quit() 
+    }
+  }]
+  }
+];
 
 let win;
 
 // index.html file path - This should be from the webpack build path
-const appUrl = `file://${__dirname}/build/index.html`;
+//const appUrl = `file://${__dirname}/build/index.html`;
 
 /**
  * Create Electron Browser window instance
@@ -12,29 +37,42 @@ const appUrl = `file://${__dirname}/build/index.html`;
  */
 function createElectronShell() {
   // Initializes the new browser window
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({ 
+    show: false, 
+    backgroundColor: '#FFF', 
+    width: 800, height: 600 
+  });
   // Load the html file into the browser window
-  win.loadURL(appUrl);
+  //win.loadURL(appUrl);
+  win.loadFile('index.html')
   // Release the variable reference when the window is closed
   win.on('closed', () => { 
     win = null 
   });
   // Opens the chrome devtool
-  win.webContents.openDevTools();
+  //win.webContents.openDevTools();
+  win.once('ready-to-show', () => {
+    win.show();
+  });
 }
 
 /**
  * Create the BrowserWindow instance and open the the main application
  * window when Electron's app module emits the ready event.
  */
-app.on('ready', createElectronShell);
+app.on('ready', () => {
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
+  createElectronShell();
+});
 
 /** 
  * The app module should exit when all the windows are closed.
  * The app.quit method should be explicitely called except on Mac machine
  */
 app.on('window-all-closed', () => {
-  if(process.platform !== 'darwin') app.quit();
+  if(process.platform !== 'darwin') 
+    app.quit();
 });
 
 /**
@@ -43,13 +81,15 @@ app.on('window-all-closed', () => {
  * when the application activate event emits
  */
 app.on('activate', () => {
-  if(win== null) createElectronShell();
+  if(win == null) 
+    createElectronShell();
 });
 
 /**
  *  Parse the command line
  */
-const args = minimist(process.argv.slice(2), {
+console.log(process.argv);
+let args = minimist(process.argv.slice(2), {
   alias: {
       h: 'help',
       v: 'version',
@@ -58,6 +98,10 @@ const args = minimist(process.argv.slice(2), {
   }
 });
 console.log(`Args: ${args}`);
+console.log(`Args.length: ${args.length}`);
+args = args._;
+console.log(`Args: ${args}`);
+console.log(`Args.length: ${args.length}`);
 for(let i = 0; i < args.length; i++) {
   let arg = args[i];
   console.log(arg);
